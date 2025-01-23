0f8c816f4cced833315a549ba8fa4b6e4eb186af
Titan antony <antonytitan96@gmail.com>
2025-01-23 13:47:05 +0300

    Chore: add a link that through icons which directs a user to my social accounts

---
+++ src/Main/SocialLinks.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function SocialLinks() {
+  return (
+    <div class='social-links'>
+      <a href='https://github.com/iconstar11' 
+        target='_blank' 
+        rel='noreferrer'>
+        <i class='fab fa-github'></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/catherine-wanjiru-283b20348?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <a
+        href="https://twitter.com/your_twitter_handle"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-twitter"></i>
+      </a>
+      
+
+    </div>
+  )
+}
