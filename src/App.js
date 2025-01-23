ec8fa3c775336172dabd43159e9c73bff161de89
Titan antony <antonytitan96@gmail.com>
2025-01-23 13:59:01 +0300

    Chore: add the necessary packages and default css stling

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Portfolio from './Main/Portfolio'
+function App() {
+  return (
+    <div className='App'>
+      <Portfolio />
+    </div>
+  )
+}
+
+export default App
